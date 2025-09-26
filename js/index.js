1bcb14a909e384e283c0c9488059604675ef6f00
UtkarshloneyaITG <UtkarshLoniya>
2025-09-26 11:52:55 +0530

    song added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 console.log('index.js running')
+const songs_container = document.querySelector('.songs-grid')
 let array_of_tracks = []
 async function fetchTracks() {
   try {
@@ -9,13 +10,27 @@ async function fetchTracks() {
       body: JSON.stringify({})
     })
     let track = await response.json()
-    array_of_tracks = track
+    array_of_tracks = track.tracks
     console.log(track.tracks)
   } catch (error) {
     console.log(error)
   }
 }
 async function renderSongs() {
-  
+  await fetchTracks()
+  songs_container.innerHTML = ''
+  console.log('=>', array_of_tracks)
+  array_of_tracks.forEach(element => {
+    let div = document.createElement('div')
+    div.setAttribute('class', 'song-card')
+    div.innerHTML = `<div class="song-image">
+                        <img src="${element.Image}" alt="" width="100%" height="100%">
+                      </div>
+                      <div class="song-info">
+                        <h4>${element.Name}</h4>
+                        <span>${element.Artists[0]}</span>
+                      </div>`
+    songs_container.appendChild(div)
+  });
 }
-fetchTracks()
+renderSongs()
